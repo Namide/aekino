0c5d05f36d0181822af1b119ad049ef4a5564335
Damien Doussaud (namide.com) <damien.git@gmail.com>
2017-06-25 15:49:27 +0200

    Sample > Relationship > Clean cube sizes

--- sample/relationship.js
+++ sample/relationship.js
@@ -1,4 +1,5 @@
 import Program from '../src/shader/material/Program'
+
 import CubeGeom from '../src/data/geom/CubeGeom'
 import SmoothCubeGeom from '../src/data/geom/SmoothCubeGeom'
 
@@ -57,52 +58,49 @@ const colorProgram = new Program(vertexShader, fragmentShader)
 
 
 // Cube data
-const cubeColors = [
-    [1.0, 0.5, 0.0, 1.0], // Front face
-    [1.0, 0.0, 0.5, 1.0], // Back face
-    [0.5, 1.0, 0.0, 1.0], // Top face
-    [0.0, 1.0, 0.5, 1.0], // Bottom face
-    [0.5, 0.0, 1.0, 1.0], // Right face
-    [0.0, 0.5, 1.0, 1.0]  // Left face
-]
-
-// Cube data
 function getColor()
 {
     return [Math.random(), Math.random(), Math.random(), 1.0]
 }
-const smoothCubeColors = [
 
+const cubeColors = [
+    getColor(), // Front face
+    getColor(), // Back face
+    getColor(), // Top face
+    getColor(), // Bottom face
+    getColor(), // Right face
+    getColor()  // Left face
+]
+
+const smoothCubeColors = [
     getColor(), getColor(),
     getColor(), getColor(),
     getColor(), getColor(),
     getColor(), getColor()
-
-
-    /*[1.0, 0.5, 0.0, 1.0],
-    [1.0, 0.0, 0.5, 1.0],
-    [0.5, 1.0, 0.0, 1.0],
-    [0.0, 1.0, 0.5, 1.0],
-    [0.5, 0.0, 1.0, 1.0],
-    [0.0, 0.5, 1.0, 1.0],
-    [1.0, 1.0, 1.0, 1.0],
-    [1.0, 1.0, 1.0, 1.0]*/
 ]
 
 
-const cubeGeom = new SmoothCubeGeom()
-cubeGeom.addColors(...smoothCubeColors)
+// GEOMETRIES
+
+const cubeGeom = new CubeGeom()
+cubeGeom.addColors(...cubeColors)
+
+const smoothCubeGeom = new SmoothCubeGeom()
+smoothCubeGeom.addColors(...smoothCubeColors)
 
 
 // Cube 1
-const cube1 = new Mesh3D(cubeGeom, colorProgram)
+const cube1 = new Mesh3D(smoothCubeGeom, colorProgram)
+cube1.transform.scale([2, 2, 2])
 cube1.addGlobalUniform(cam3D)
 scene.addMesh(cube1)
 
 
 // Cube 2
+
+
 const cube2 = new Mesh3D(cubeGeom, colorProgram)
-cube2.transform.translate([-3, 0, 0])
+cube2.transform.translate([-1.5, 0, 0])
 cube2.transform.scale([0.5, 0.5, 0.5])
 cube2.transform.attach(cube1.transform)
 cube2.addGlobalUniform(cam3D)
@@ -110,8 +108,8 @@ scene.addMesh(cube2)
 
 
 // Cube 3
-const cube3 = new Mesh3D(cubeGeom, colorProgram)
-cube3.transform.translate([0, 0, -3])
+const cube3 = new Mesh3D(smoothCubeGeom, colorProgram)
+cube3.transform.translate([0, 0, -1.5])
 cube3.transform.scale([0.5, 0.5, 0.5])
 cube3.transform.attach(cube2.transform)
 cube3.addGlobalUniform(cam3D)
